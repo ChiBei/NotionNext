ade570c5307fb3cbcad2afac79777c7074492f00
ChiBei <1974913861@qq.com>
2025-02-27 14:43:07 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -29,9 +29,9 @@ const CONFIG = {
   HEO_HERO_COVER_TITLE: '随便逛逛',
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
-  HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
-  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
+  HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/COS' },
+  HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/画作' },
+  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/直播' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
@@ -40,13 +40,13 @@ const CONFIG = {
 
   // 右侧个人资料卡牌欢迎语，点击可自动切换
   HEO_INFOCARD_GREETINGS: [
-    '你好！我是',
-    '🔍 分享与热心帮助',
-    '🤝 专修交互与设计',
-    '🏃 脚踏实地行动派',
-    '🏠 智能家居小能手',
-    '🤖️ 数码科技爱好者',
-    '🧱 团队小组发动机'
+    '你好！我流窜于',
+    '🍽️ 生活区',
+    '🎮 游戏区',
+    '📝 学习区',
+    '🧑‍💻 科技区',
+    '📦 菜鸟裹裹区',
+    // '🧱 团队小组发动机'
   ],
 
   // 个人资料底部按钮
