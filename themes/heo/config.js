3b5a63a36f8225815fdea3994fb8f56b54d0a72d
ChiBei <1974913861@qq.com>
2025-02-27 22:27:15 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -62,11 +62,11 @@ const CONFIG = {
   // 用户技能图标
   HEO_GROUP_ICONS: [
     {
-      title_1: 'Live',
-      img_1: '/images/heo/1-1.png',
-      color_1: '#989bf8',
-      title_2: 'ComicCon',
-      img_2: '/images/heo/2023e0ded7b724a39f12d59c3dc8fbdc7cbe074202.webp',
+      title_1: 'ComicCon',
+      img_1: '/images/heo/2-1.png',
+      color_1: '#ffffff',
+      title_2: 'Live',
+      img_2: '/images/heo/1-1.webp',
       color_2: '#ffffff'
     }
     // ,
