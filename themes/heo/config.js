793dc70bdec80ca173cef6426bc7a3f6955b66ed
ChiBei <1974913861@qq.com>
2025-02-27 21:57:58 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -63,7 +63,7 @@ const CONFIG = {
   HEO_GROUP_ICONS: [
     {
       title_1: 'Live',
-      img_1: 'https://www.jianguoyun.com/p/DfgD1n4QyZv6Chifq-0FIAA',
+      img_1: 'https://www.jianguoyun.com/p/DeyDw_wQyZv6Chivq-0FIAA',
       color_1: '#989bf8',
       title_2: 'ComicCon',
       img_2: '/images/heo/2023e0ded7b724a39f12d59c3dc8fbdc7cbe074202.webp',
