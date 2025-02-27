7ff3544fd2974eb233e9852eb6544336aac896a6
ChiBei <1974913861@qq.com>
2025-02-27 14:55:24 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -52,8 +52,10 @@ const CONFIG = {
   // 个人资料底部按钮
   HEO_INFO_CARD_URL1: '/about',
   HEO_INFO_CARD_ICON1: 'fas fa-user',
-  HEO_INFO_CARD_URL2: 'https://github.com/tangly1024',
+  HEO_INFO_CARD_URL2: 'https://space.bilibili.com/567031112',
   HEO_INFO_CARD_ICON2: 'fab fa-bilibili',
+  HEO_INFO_CARD_URL2: 'https://space.bilibili.com/567031112',
+  HEO_INFO_CARD_ICON2: 'fab fa-tiktok',
   HEO_INFO_CARD_URL3: 'https://space.bilibili.com/567031112',
   HEO_INFO_CARD_TEXT3: '了解更多',
 
