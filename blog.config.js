caa29fd7e7dedf29f1cfc041588a62a89b7ff9a0
ChiBei <1974913861@qq.com>
2025-03-03 14:59:18 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -14,7 +14,6 @@ const BLOG = {
   APPEARANCE: process.env.NEXT_PUBLIC_APPEARANCE || 'light', // ['light', 'dark', 'auto'], // light 日间模式 ， dark夜间模式， auto根据时间和主题自动夜间模式
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
-  
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Ted', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '记录生活', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://tedmosby.top/', // 网站地址
